c0a061bcee10028a96d433e7d56f53a2bfa9996e
hickatheworld <cc@hicka.world>
2022-12-04 15:21:53 +0100

    complete day 3 part 2 - quite easy day

--- day3.py
+++ day3.py
@@ -1,11 +1,21 @@
 f = open('./inputs/day3.txt')
 lines = [l[:-1] for l in f.readlines()]
-
-s = 0
 alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# Part 1
+s = 0
 for l in lines:
     for c in l[:len(l)//2]:
         if c in l[len(l)//2:]:
+            s += alphabet.index(c) + 1
+            break
+print(s)
+
+# Part 2
+s = 0
+for i in range(0, len(lines), 3):
+    for c in lines[i]:
+        if c in lines[i + 1] and c in lines[i + 2]:
             s += alphabet.index(c) + 1
             break
 print(s)
