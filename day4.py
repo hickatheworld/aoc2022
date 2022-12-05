1447079370a0a4745988051cc478abf9b92a9cda
hickatheworld <cc@hicka.world>
2022-12-05 10:56:07 +0100

    complete day 4 part 2

--- day4.py
+++ day4.py
@@ -16,3 +16,19 @@ for l in lines:
     if ranges[x][0] <= ranges[y][0] and ranges[x][1] >= ranges[y][1]:
         s+=1
 print(s)
+
+# Part 2
+s = 0
+for l in lines: 
+    split = l.split(',')
+    ranges = [
+        [int(n) for n in split[0].split('-')],
+        [int(n) for n in split[1].split('-')]
+    ]
+    # x is the index of the section assigment that starts first
+    x = 0 if ranges[0][0] <= ranges[1][0] else 1
+    # y is the other index
+    y = (x + 1) % 2
+    if ranges[y][0] <= ranges[x][1]:
+        s+=1
+print(s)
